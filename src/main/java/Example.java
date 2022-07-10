21e59eb8b0aa18d6b58eca8c32c5fdea9ed2b4b7
ThorstiFinki <thorsten_finken@t-online.de>
2022-07-10 09:56:45 +0200

    Erster Test

---
+++ src/main/java/Example.java
@@ -0,0 +1,5 @@
+public class Example {
+
+    public static void giveAnswer(){
+        System.out.println("Ja hat er");
+    }}
---
+++ src/main/java/center.java
@@ -0,0 +1,13 @@
+
+
+
+
+public class center {
+    public static void main(String[] args) {
+
+        System.out.println("Hat Thorsti Motivation?");
+        Example.giveAnswer();
+
+
+    }
+}
