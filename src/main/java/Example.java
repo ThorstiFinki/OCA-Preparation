834f3b596e0c2c33e7811bdcbab147df609caae2
ThorstiFinki <105248638+ThorstiFinki@users.noreply.github.com>
2022-07-11 22:28:39 +0200

    Erster Test

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -1,12 +1,22 @@
 public class Example {
 
+    public Example() {
+    System.out.println("Example-constructor call");
+    }
+
     // nicht static
      public void giveAnswer(){
-        System.out.println("Ja hat er");
+        System.out.println("Example- method call");
     }
+    int weiteresTestInt =5;
     // static
     public static void giveSecondAnswer(){
         System.out.println("Alles statisch");
-    }}
+    }
+    {System.out.println("Example-initialization call");}
+
+
+}
 
 class Test { }
+
--- src/main/java/center.java
+++ src/main/java/center.java
@@ -7,9 +7,19 @@ public class center {
 
         System.out.println("Hat Thorsti Motivation?");
         Example example = new Example();
-        example.giveAnswer();
+        //nicht statisch
+                example.giveAnswer();
+        //statisch
         Example.giveSecondAnswer();
+        int testint = 3, $testint$ = 3, _testint_ = 3, T2 =3;
+        // int 2T = 3, !testint! = 3;
+        System.out.printf( "%d, %d, %d, %d %n" ,testint,$testint$,_testint_,T2);
+
+        }
 
 
-    }
+
 }
+
+
+
